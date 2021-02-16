3832535028becd1a24b036c85fb4cac905e1bd66
Edwin Vlieg <edwin@moneybird.com>
2021-02-16 14:32:46 +0100

    Implement shorthands

--- lib/active_date_range/date_range.rb
+++ lib/active_date_range/date_range.rb
@@ -2,13 +2,32 @@
 
 module ActiveDateRange
   class DateRange < Range
-    def initialize(begin_date, end_date)
+    SHORTHANDS = {
+      this_month: -> { DateRange.new(Time.zone.today.all_month) },
+      previous_month: -> { DateRange.new(1.month.ago.to_date.all_month) },
+      next_month: -> { DateRange.new(1.month.from_now.to_date.all_month) },
+      this_quarter: -> { DateRange.new(Time.zone.today.all_quarter) },
+      previous_quarter: -> { DateRange.new(3.months.ago.to_date.all_quarter) },
+      next_quarter: -> { DateRange.new(3.months.from_now.to_date.all_quarter) },
+      this_year: -> { DateRange.new(Time.zone.today.all_year) },
+      previous_year: -> { DateRange.new(12.months.ago.to_date.all_year) },
+      next_year: -> { DateRange.new(12.months.from_now.to_date.all_year) }
+    }.freeze
+
+    class << self
+      SHORTHANDS.each do |method, range|
+        define_method(method, range)
+      end
+    end
+
+    def initialize(begin_date, end_date = nil)
+      begin_date, end_date = begin_date.begin, begin_date.end if begin_date.kind_of?(Range)
+      begin_date = begin_date.to_date if begin_date.kind_of?(Time)
+      end_date = end_date.to_date if end_date.kind_of?(Time)
+
       raise InvalidDateRange, "Date range invalid, begin should be a date" unless begin_date.kind_of?(Date)
       raise InvalidDateRange, "Date range invalid, end should be a date" unless end_date.kind_of?(Date)
       raise InvalidDateRange, "Date range invalid, begin #{begin_date} is after end #{end_date}" if begin_date >= end_date
-
-      begin_date = begin_date.to_date if begin_date.kind_of?(Time)
-      end_date = end_date.to_date if end_date.kind_of?(Time)
 
       super(begin_date, end_date)
     end
--- test/active_date_range/date_range_test.rb
+++ test/active_date_range/date_range_test.rb
@@ -11,6 +11,10 @@ class ActiveDateRangeDateRangeTest < ActiveSupport::TestCase
     assert_kind_of Date, range.end
   end
 
+  def test_initialize_range
+    assert_kind_of Range, ActiveDateRange::DateRange.new(Date.new(2021, 1, 1)..Date.new(2021, 12, 31))
+  end
+
   def test_initialize_no_dates
     assert_raises(ActiveDateRange::InvalidDateRange) { ActiveDateRange::DateRange.new(1, 2) }
     assert_raises(ActiveDateRange::InvalidDateRange) { ActiveDateRange::DateRange.new(Date.new(2021, 1, 1), 2) }
@@ -20,5 +24,17 @@ class ActiveDateRangeDateRangeTest < ActiveSupport::TestCase
   def test_initialize_end_before_start
     assert_raises(ActiveDateRange::InvalidDateRange) { ActiveDateRange::DateRange.new(Date.new(2021, 12, 31), Date.new(2021, 1, 1)) }
     assert_raises(ActiveDateRange::InvalidDateRange) { ActiveDateRange::DateRange.new(Date.new(2021, 1, 1), Date.new(2021, 1, 1)) }
+  end
+
+  def test_shorthands
+    assert_equal Date.today.all_month, ActiveDateRange::DateRange.this_month
+    assert_equal 1.month.ago.to_date.all_month, ActiveDateRange::DateRange.previous_month
+    assert_equal 1.month.from_now.to_date.all_month, ActiveDateRange::DateRange.next_month
+    assert_equal Date.today.all_quarter, ActiveDateRange::DateRange.this_quarter
+    assert_equal 3.months.ago.to_date.all_quarter, ActiveDateRange::DateRange.previous_quarter
+    assert_equal 3.months.from_now.to_date.all_quarter, ActiveDateRange::DateRange.next_quarter
+    assert_equal Date.today.all_year, ActiveDateRange::DateRange.this_year
+    assert_equal 12.months.ago.to_date.all_year, ActiveDateRange::DateRange.previous_year
+    assert_equal 12.months.from_now.to_date.all_year, ActiveDateRange::DateRange.next_year
   end
 end
