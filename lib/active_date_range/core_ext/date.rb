a2a9cf28f80e8eedb1faacc13eabd707b132c768
Edwin Vlieg <edwin@moneybird.com>
2021-02-19 14:11:46 +0100

    Documentation to core ext

--- lib/active_date_range/core_ext/date.rb
+++ lib/active_date_range/core_ext/date.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Date
+  # Returns the number of the quarter for this date
+  #
+  #   Date.new(2021, 1, 1) # => 1
   def quarter
     case month
     when 1..3
@@ -14,10 +17,16 @@ class Date
     end
   end
 
+  # Shifts the date to the next quarter
+  #
+  #   Date.new(2021, 1, 1).next_quarter # => Date.new(2021, 4, 1)
   def next_quarter(n = 1)
     self >> (n * 3)
   end
 
+  # Shifts the date to the previous quarter
+  #
+  #   Date.new(2021, 4, 1).next_quarter # => Date.new(2021, 1, 1)
   def prev_quarter(n = 1)
     self << (n * 3)
   end
--- lib/active_date_range/core_ext/integer.rb
+++ lib/active_date_range/core_ext/integer.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Integer
+  # Returns Duration instance matching the number of quarters provided
+  #
+  #   2.quarters # => 6 months
   def quarters
     self * 3.months
   end
