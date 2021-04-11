ed697a67070ae62b13f8300a3a7e4a8dc24a8086
Edwin Vlieg <edwin@moneybird.com>
2021-04-11 16:07:13 +0200

    next_quarter and prev_quarter are already available in ActiveSupport

--- lib/active_date_range/core_ext/date.rb
+++ lib/active_date_range/core_ext/date.rb
@@ -3,22 +3,8 @@
 class Date
   # Returns the number of the quarter for this date
   #
-  #   Date.new(2021, 1, 1) # => 1
+  #   Date.new(2021, 1, 1).quarter # => 1
   def quarter
     (month / 3.0).ceil
-  end
-
-  # Shifts the date to the next quarter
-  #
-  #   Date.new(2021, 1, 1).next_quarter # => Date.new(2021, 4, 1)
-  def next_quarter(n = 1)
-    self >> (n * 3)
-  end
-
-  # Shifts the date to the previous quarter
-  #
-  #   Date.new(2021, 4, 1).next_quarter # => Date.new(2021, 1, 1)
-  def prev_quarter(n = 1)
-    self << (n * 3)
   end
 end
